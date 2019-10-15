a483affdb22c9c6a72ff0075492a5bf3d7294784
salonibarvalia <35100783+salonibarvalia@users.noreply.github.com>
2019-10-15 15:16:39 +0530

    code files uploaded

---
+++ Section 5/Code.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AsyncStreams
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("Press any key when ready");
+            Console.ReadLine();
+            await foreach (var e in GetDataAsync())
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static IEnumerable<string> GetData()
+        {
+            yield return "data A";
+            yield return "data B";
+        }
+
+        static async IAsyncEnumerable<string> GetDataAsync()
+        {
+            await Task.Delay(1500);
+            yield return "data A";
+            await Task.Delay(2000);
+            yield return "data B";
+        }
+    }
+}
